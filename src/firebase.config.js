06f5f333084ec4e5dd93f5f92b26f0264091a360
Mukesh-Sonu <mukeshmuki761@gmail.com>
2022-03-07 20:33:59 +0530

    created a dummy data in firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6CdTlFHDD039cBYWcLHYYdlXdD5t2nU0",
+  authDomain: "house-marketplace-app-cf059.firebaseapp.com",
+  projectId: "house-marketplace-app-cf059",
+  storageBucket: "house-marketplace-app-cf059.appspot.com",
+  messagingSenderId: "889333341347",
+  appId: "1:889333341347:web:88809dbdc21d59cbe3f316",
+};
+
+// Initialize Firebase
+
+export const db = getFirestore();
